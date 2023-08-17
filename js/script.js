1cc98d9404a8112fa105cb1a0a0a363186426ad1
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-17 20:20:13 -0300

    jys

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 
-const img = {
-  1: ""
-}
+let sliderInner = document.querySelector(".slider-inner");
+
+let img = document.querySelectorAll("#paisaje-img")
+
+let i = 1;
+setInterval(() => {
+  let parcentage;
+  let index;
+  if (i >= img.length) {
+    parcentage = i * -100;
+    index = "i--"
+  }
+  else if (i <= 7) {
+    parcentage = i * -100
+    index = "i++"
+  }
+  sliderInner.style.transform = `translate(${parcentage}%)`;
+  if (index === "i++")  i++ 
+  else  i--
+}, 3000);
