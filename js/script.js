c9d48b30e320f830338c91c649f2d2b552376dfd
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-18 22:13:38 -0300

    jys

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*
 let sliderInner = document.querySelector(".slider-inner");
 
 let img = document.querySelectorAll("#paisaje-img")
@@ -12,3 +12,39 @@ setInterval(() => {
     i = 1;
   }
 }, 4000);
+*/
+
+const slider = document.querySelector('.slider');
+const sliderList = slider.querySelector('ul');
+const sliderItems = slider.querySelectorAll('li');
+const sliderCount = sliderItems.length;
+const sliderWidth = slider.clientWidth;
+
+let currentIndex = 0;
+
+function shiftSlider() {
+  sliderList.classList.add('shift');
+  setTimeout(() => {
+    sliderList.classList.remove('shift');
+    sliderList.appendChild(sliderItems[currentIndex]);
+  }, 500);
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % sliderCount;
+  shiftSlider();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + sliderCount) % sliderCount;
+  shiftSlider();
+}
+
+setInterval(nextSlide, 5000);
+
+const nextButton = document.getElementById("siguiente");
+nextButton.addEventListener('click', nextSlide);
+
+
+const prevButton = document.getElementById('anterior');
+prevButton.addEventListener('click', prevSlide);
