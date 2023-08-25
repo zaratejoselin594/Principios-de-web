6d42818edda0800316bcb965570173228d75f49e
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-25 18:29:13 -0300

    jys

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Define the origin and destination
+const origin = "New York, NY";
+const destination = "Los Angeles, CA";
+
+// Create a DirectionsService object
+const directionsService = new google.maps.DirectionsService();
+
+// Define the travel mode
+const travelMode = google.maps.TravelMode.DRIVING;
+
+// Define the request object
+const request = {
+  origin: origin,
+  destination: destination,
+  travelMode: travelMode,
+};
+
+// Call the DirectionsService object to calculate the distance
+directionsService.route(request, function (response, status) {
+  if (status == google.maps.DirectionsStatus.OK) {
+    // Get the distance value in meters
+    const distance = response.routes[0].legs[0].distance.value;
+
+    // Convert the distance to kilometers
+    const distanceInKm = distance * 0.001;
+
+    // Calculate the cost of the trip
+    const cost = distanceInKm * 250;
+
+    console.log("Distance: " + distanceInKm + " km");
+    console.log("Cost: $" + cost);
+  } else {
+    console.log("Error: " + status);
+  }
+});
