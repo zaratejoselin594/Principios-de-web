753659407befc0fd191a4376cde2626dfadbdafe
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-29 21:04:21 -0300

    jys

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,11 @@ const mql = matchMedia("(max-width: 700px)");
 
 mql.addEventListener("change", () => {
   if (mql.matches) {
-    nav.className.replace("item-nav", "responsiveNav");
-    icon.className.replace("icon-menu", "responsive-icon")
-  }
+    nav.classList.replace("item-nav", "responsiveNav");
+    icon.classList.replace("icon-menu", "responsive-icon");
+  };
   else if (nav.className == "responsiveNav") {
-    nav.className.replace("responsiveNav", "nav");
-    icon.className.replace("responsive-icon", "icon-menu")
+    nav.classList.replace("responsiveNav", "nav");
+    icon.classList.replace("responsive-icon", "icon-menu");
   }
 })
